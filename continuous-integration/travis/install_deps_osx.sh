32444cf3af717285a91c22665d0d2abd76f2e501
Leyton Ho <leytonh@leytonhs-MacBook-Pro-HQ.local>
2019-08-02 18:02:10 -0700

    add .travis.yml & dependencies for builds

---
+++ continuous-integration/travis/install_deps_osx.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -x
+
+echo "Installing Wake"
+
+git clone https://github.com/sifive/wake.git
+make -C wake
+export PATH=$PATH:$PWD/wake/bin
+
+
+echo "Installing Wit"
+
+git clone https://github.com/sifive/wit.git
+export PATH=$PATH:$PWD/wit
