a617e920a7dd4f58715bf0e3be6312b8be4ace09
Leyton Ho <leytonh@leytonhs-MacBook-Pro-HQ.local>
2019-08-05 10:34:58 -0700

    checkout macOS wake fix commit

--- continuous-integration/travis/install_deps_osx.sh
+++ continuous-integration/travis/install_deps_osx.sh
@@ -3,9 +3,9 @@
 echo "Installing Wake"
 
 git clone https://github.com/sifive/wake.git
+git -C wake checkout a539d6700afd02dd0f83e4c887f98650e7bdebb1 # commit of mac os fix
 make -C wake
 export PATH=$PATH:$PWD/wake/bin
-
 
 echo "Installing Wit"
 
